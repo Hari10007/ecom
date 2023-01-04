9c6ae4c7b91beb681650e44a8c4a6928dde5875f
hari <harikrishnansr1007@gmail.com>
2023-01-04 15:14:05 +0530

    update2 commit

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -167,7 +167,7 @@ if DEBUG:
         os.path.join(BASE_DIR, 'static')
     ]
 else:
-    STATIC_ROOT= os.path.join(PROJECT_DIR,'static/')
+    STATIC_ROOT= os.path.join(BASE_DIR,'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
